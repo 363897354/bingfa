1517a9fa2ce766ee22c38cc69bf3e662857f6fc9
363897354 <363897354@qq.com>
2020-02-29 12:07:31 +0800

    Update SellApplication.java

--- src/main/SellApplication.java
+++ src/main/SellApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SellApplication {
 
-   //hiia
+   //hiiabvv
 	public static void main(String[] args) {
 		SpringApplication.run(SellApplication.class, args);
 	}
