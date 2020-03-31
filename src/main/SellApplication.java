f7b7feef55d33b087c76ff0a600b59667a59b650
363897354 <363897354@qq.com>
2020-03-31 09:08:04 +0800

    修改

--- src/main/SellApplication.java
+++ src/main/SellApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SellApplication {
 
-   //hiiabvv
+   //hiiabvv啊啊啊
 	public static void main(String[] args) {
 		SpringApplication.run(SellApplication.class, args);
 	}
